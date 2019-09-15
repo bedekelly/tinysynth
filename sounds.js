1067070944dab67051370c45dfe309dd6bf28067
Bede Kelly <me@bede.io>
2019-09-15 03:28:39 +0100

    done-filter

--- sounds.js
+++ sounds.js
@@ -1,14 +1,25 @@
 const soundButton = document.querySelector("#sound-button");
-
+const filterSlider = document.querySelector("#muffler-slider");
 
 let context;
 let oscillator;
+let filter;
+
+
+function createFilter() {
+  filter = context.createBiquadFilter();
+  filter.type = "lowpass";
+  filter.Q.setValueAtTime(1.5, 0);
+  filter.frequency.setValueAtTime(400, 0);
+  filter.connect(context.destination);
+}
 
 
 async function toggleSound() {
   if (!context) {
     context = await new AudioContext();
     await context.resume();
+    createFilter();
   }
 
   if (oscillator) {
@@ -16,9 +27,9 @@ async function toggleSound() {
     oscillator = null;
   } else {
     oscillator = context.createOscillator();
-    oscillator.type = "sine";
+    oscillator.type = "sawtooth";
     oscillator.frequency.setValueAtTime(0, 440);
-    oscillator.connect(context.destination);
+    oscillator.connect(filter);
     oscillator.start(0);
   }
 
@@ -30,6 +41,13 @@ function keyListener(event) {
     toggleSound();
   }
 }
+
+
+filterSlider.addEventListener("input", event => {
+  const value = parseFloat(event.target.value);
+  const frequency = value * 80;
+  filter.frequency.setValueAtTime(frequency, 0);
+});
 
 
 soundButton.addEventListener("click", toggleSound);
